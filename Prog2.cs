275ba7dd98e1d18597a99f8723d1b38d314d055f
biharib27 <biharibalazs17@gmail.com>
2025-11-28 11:06:36 +0100

    Add files via upload

---
+++ Prog2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+
+List<byte> csapadek = new List<byte>();
+csapadek = File.ReadAllLines("C:\\adatok\\csapadek.txt").Select(sor => Convert.ToByte(sor)).ToList();
+
+for (int i = 0; i < csapadek.Count; i++)
+{
+
+    if (csapadek[i] > 30)
+    {
+        Console.WriteLine("Túl sok csapadék esett");
+
+    }
+    if (i > 170 && i < 236)
+    {
+        if (csapadek[i] < 1 && csapadek[1] > 19)
+        {
+            Console.WriteLine("Túl sok csapadék esett");
+        }
+
+
+    }
+
+}
+if (csapadek.Count > 365)
+{
+        Console.WriteLine("Több adat van, mint egy év");
+}
