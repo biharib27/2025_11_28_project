3ebc622745cd7603cba8a62c4a415c6b4a9a0cc7
biharib27 <biharibalazs17@gmail.com>
2025-11-28 11:05:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+
+List<byte> csapadek = new List<byte>();
+Random random = new Random();
+for (int i = 0; i < 366; i++)
+{
+    if(i>170 && i < 235)
+    {
+        int arany =random.Next(1, 17);
+        if(arany > 1)
+        {
+            csapadek.Add(0);       
+         }
+        else
+        {
+            csapadek.Add((byte)random.Next(1, 19));
+        }
+
+    }
+    else
+    {
+        int arany2 =random.Next(1, 7);
+        if (arany2 > 1) {
+            csapadek.Add(0);
+        }
+        else
+        {
+            csapadek.Add((byte)random.Next(1, 30));
+        }
+    }
+}
+File.WriteAllLines("C:\\adatok\\csapadek.txt", csapadek.Select(szam => szam + ""));
+
+    
